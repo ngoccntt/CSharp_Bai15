1ecb086293aa3e7a828b8f12535999fcbd8e73b4
ngoccntt <tranquocduong08012001@gmail.com>
2023-09-25 15:35:25 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace CSharp_Bai15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblHienThi = new System.Windows.Forms.Label();
+            this.picTurnOff = new System.Windows.Forms.PictureBox();
+            this.picTurnOn = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picTurnOff)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTurnOn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(100, 29);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(246, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // lblHienThi
+            // 
+            this.lblHienThi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblHienThi.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHienThi.Location = new System.Drawing.Point(41, 315);
+            this.lblHienThi.Name = "lblHienThi";
+            this.lblHienThi.Size = new System.Drawing.Size(543, 39);
+            this.lblHienThi.TabIndex = 3;
+            // 
+            // picTurnOff
+            // 
+            this.picTurnOff.BackgroundImage = global::CSharp_Bai15.Properties.Resources.pngtree_tunnel_rapid_background_image_jpg_image_2802781;
+            this.picTurnOff.Location = new System.Drawing.Point(41, 53);
+            this.picTurnOff.Name = "picTurnOff";
+            this.picTurnOff.Size = new System.Drawing.Size(543, 257);
+            this.picTurnOff.TabIndex = 2;
+            this.picTurnOff.TabStop = false;
+            this.picTurnOff.Click += new System.EventHandler(this.picTurnOff_Click);
+            this.picTurnOff.MouseHover += new System.EventHandler(this.picTurnOff_MouseHover);
+            // 
+            // picTurnOn
+            // 
+            this.picTurnOn.BackgroundImage = global::CSharp_Bai15.Properties.Resources.pngtree_tunnel_rapid_background_image_jpg_image_280278;
+            this.picTurnOn.Location = new System.Drawing.Point(41, 53);
+            this.picTurnOn.Name = "picTurnOn";
+            this.picTurnOn.Size = new System.Drawing.Size(543, 257);
+            this.picTurnOn.TabIndex = 2;
+            this.picTurnOn.TabStop = false;
+            this.picTurnOn.Click += new System.EventHandler(this.picTurnOn_Click);
+            this.picTurnOn.MouseHover += new System.EventHandler(this.picTurnOn_MouseHover);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 366);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Designed by:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(149, 366);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 22);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Ngọc Designer";
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(299, 357);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 30);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "E&xit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.CancelButton = this.button1;
+            this.ClientSize = new System.Drawing.Size(593, 399);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblHienThi);
+            this.Controls.Add(this.picTurnOff);
+            this.Controls.Add(this.picTurnOn);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "frmLight";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picTurnOff)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTurnOn)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.PictureBox picTurnOn;
+        private System.Windows.Forms.Label lblHienThi;
+        private System.Windows.Forms.PictureBox picTurnOff;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_Bai15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void picTurnOn_Click(object sender, EventArgs e)
+        {
+            picTurnOn.Visible = false;
+            picTurnOff.Visible = true;
+            lblHienThi.Text = txtName.Text + " Turn Off the Light ,please!”";
+        }
+
+        private void picTurnOff_Click(object sender, EventArgs e)
+        {
+            picTurnOn.Visible = true;
+            picTurnOff.Visible = false;
+            lblHienThi.Text = txtName.Text + " Turn On the Light ,please!”";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtName.Text = "";
+            lblHienThi.Text = txtName.Text + " ,Turn Off the Light ,please!”";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+            
+        }
+
+        private void picTurnOff_MouseHover(object sender, EventArgs e)
+        { 
+            toolTip1.Show("Click Me to Turn ON the Light!", picTurnOff);
+        }
+
+        private void picTurnOn_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.Show("Click Me to Turn OFF the Light!", picTurnOn);
+        }
+    }
+}
